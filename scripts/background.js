6b44f1f5c1c70be45a068bb2d912ae05593a1896
KazaKazan <ahmetfiratusta@hotmail.com>
2023-02-18 15:28:12 +0300

    Add a saved state and a function to load the state from browser storage to background.js

--- scripts/background.js
+++ scripts/background.js
@@ -1,9 +1,18 @@
-// Reddit Sidebar Toggle.
-// Created by Dylan Araps.
 /* eslint-disable no-undef */
+
+browser.storage.onChanged.addListener(loadState);
+
+let state = true;
 
 chrome.browserAction.onClicked.addListener(function() {
     chrome.tabs.executeScript({
         file: "scripts/reddit.js"
     });
 });
+
+async function loadState(sidebarState) {
+    let savedState = await browser.storage.local.get("storedState);
+    if(savedState != undefined){
+        state = savedState;
+    };
+}
